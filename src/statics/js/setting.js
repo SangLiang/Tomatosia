ded4019359dba19ef2af79275acdc3ebb6a60ae9
DESKTOP-A628ISE\桑量 <378305868@qq.com>
2018-11-07 17:37:31 +0800

    设置列表

--- src/statics/js/setting.js
+++ src/statics/js/setting.js
@@ -12,12 +12,23 @@ $(function(){
   $(".longRestTime").attr("value", data.longRestTime);
   $(".tomatoCountToLongRest").attr("value", data.tomatoCountToLongRest);
 
+  // 提交修改
   $(".changeButton").click(function(){
-    localStorage.setItem("baseTime",$(".baseTime").val());
-    localStorage.setItem("restTime",$(".restTime").val());
-    localStorage.setItem("longRestTime",$(".longRestTime").val());
-    localStorage.setItem("tomatoCountToLongRest",$(".tomatoCountToLongRest").val());
-    window.location.href="./index.html";
-    alert("修改成功");
+    var _baseTime = parseInt($(".baseTime").val());
+    var _restTime = parseInt($(".restTime").val());
+    var _longRestTime = parseInt($(".longRestTime").val());
+    var _tomatoCountToLongRest = parseInt($(".tomatoCountToLongRest").val());
+
+    if(!isNaN(_baseTime) && !isNaN(_restTime) && !isNaN(_longRestTime) && !isNaN(_tomatoCountToLongRest)){
+      localStorage.setItem("baseTime",_baseTime);
+      localStorage.setItem("restTime",_restTime);
+      localStorage.setItem("longRestTime",_longRestTime);
+      localStorage.setItem("tomatoCountToLongRest",_tomatoCountToLongRest);
+      window.location.href="./index.html";
+      alert("修改成功");
+    } else {
+      alert ("输入有误");
+    }
+    
   });
 });
